a82d376b02a236d3275c8efcc17b87d17ce53943
Ken Duck <ken@qracorp.com>
2015-08-03 22:19:45 -0400

    Added builder stubs. These provide basic builder functionality.

---
+++ plugins/net.ossindex.eclipse.common/src/net/ossindex/eclipse/common/builder/CBuildVisitor.java
@@ -0,0 +1,105 @@
+/**
+ *	Copyright (c) 2015 Vör Security Inc.
+ *	All rights reserved.
+ *	
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *	    * Redistributions of source code must retain the above copyright
+ *	      notice, this list of conditions and the following disclaimer.
+ *	    * Redistributions in binary form must reproduce the above copyright
+ *	      notice, this list of conditions and the following disclaimer in the
+ *	      documentation and/or other materials provided with the distribution.
+ *	    * Neither the name of the <organization> nor the
+ *	      names of its contributors may be used to endorse or promote products
+ *	      derived from this software without specific prior written permission.
+ *	
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ossindex.eclipse.common.builder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+
+/** Build visitor that identifies appropriate files, collects interesting
+ * build information, and calls a method that will be useful for sub-classing.
+ * 
+ * @author Ken Duck
+ *
+ */
+public class CBuildVisitor implements IResourceVisitor, IResourceDeltaVisitor
+{
+	/**
+	 * Acceptable C/C++ file extensions.
+	 */
+	private static Set<String> extensions = new HashSet<String>();
+	
+	/**
+	 * FIXME: There is probably an eclipse specific way to get acceptable file extensions.
+	 */
+	static
+	{
+		extensions.add(".c");
+		extensions.add(".cc");
+		extensions.add(".cpp");
+		extensions.add(".cxx");
+	}
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.resources.IResourceVisitor#visit(org.eclipse.core.resources.IResource)
+	 */
+	@Override
+	public boolean visit(IResource resource) throws CoreException
+	{
+		if(isCppCompilationUnit(resource))
+		{
+			System.out.println("  C VISIT: " + resource);
+		}
+		return true;
+	}
+
+	/** Return true if this is a C/C++ compilation unit
+	 * 
+	 * @param resource
+	 * @return
+	 */
+	private boolean isCppCompilationUnit(IResource resource)
+	{
+		if(resource instanceof IFile)
+		{
+			String name = resource.getName();
+			for(String extension: extensions)
+			{
+				if(name.endsWith(extension)) return true;
+			}
+		}
+		return false;
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.resources.IResourceDeltaVisitor#visit(org.eclipse.core.resources.IResourceDelta)
+	 */
+	@Override
+	public boolean visit(IResourceDelta delta) throws CoreException
+	{
+		IResource resource = delta.getResource();
+		return visit(resource);
+	}
+
+}
---
+++ plugins/net.ossindex.eclipse.common/src/net/ossindex/eclipse/common/builder/CommonBuilder.java
@@ -0,0 +1,122 @@
+/**
+ *	Copyright (c) 2015 Vör Security Inc.
+ *	All rights reserved.
+ *	
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *	    * Redistributions of source code must retain the above copyright
+ *	      notice, this list of conditions and the following disclaimer.
+ *	    * Redistributions in binary form must reproduce the above copyright
+ *	      notice, this list of conditions and the following disclaimer in the
+ *	      documentation and/or other materials provided with the distribution.
+ *	    * Neither the name of the <organization> nor the
+ *	      names of its contributors may be used to endorse or promote products
+ *	      derived from this software without specific prior written permission.
+ *	
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ossindex.eclipse.common.builder;
+
+import java.util.Map;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+/** 
+ * 
+ * @author Ken Duck
+ *
+ */
+public abstract class CommonBuilder extends IncrementalProjectBuilder
+{
+	public CommonBuilder()
+	{
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.resources.IncrementalProjectBuilder#build(int, java.util.Map, org.eclipse.core.runtime.IProgressMonitor)
+	 */
+	@Override
+	protected IProject[] build(int kind, Map<String, String> args, IProgressMonitor monitor) throws CoreException
+	{
+		System.out.println("Custom builder triggered");  
+
+		// get the project to build  
+		getProject();  
+
+		if(kind == FULL_BUILD)
+		{
+			fullBuild(monitor);
+		}
+		else
+		{
+			IResourceDelta delta = getDelta(getProject());
+			if (delta == null) {
+				fullBuild(monitor);
+			} else {
+				incrementalBuild(delta, monitor);
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param monitor
+	 */
+	private void fullBuild(IProgressMonitor monitor)
+	{
+		System.out.println("FULL BUILD");
+		try
+		{
+			getProject().accept(getBuildVisitor());
+		}
+		catch (CoreException e)
+		{
+			System.err.println("Exception performing full build");
+			e.printStackTrace();
+		}
+	}
+
+	/** A Build Visitor is a class that actually performs the build operations.
+	 * 
+	 * @return
+	 */
+	protected abstract IResourceVisitor getBuildVisitor();
+
+	/**
+	 * 
+	 * @param delta
+	 * @param monitor
+	 */
+	private void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor)
+	{
+		System.out.println("INCREMENTAL BUILD");
+		try
+		{
+			delta.accept(getDeltaVisitor());
+		}
+		catch (CoreException e)
+		{
+			System.err.println("Exception performing incremental build");
+			e.printStackTrace();
+		}
+	}
+
+	protected abstract IResourceDeltaVisitor getDeltaVisitor();
+}
